b7ba1422ca2104b4ff11386b0f19866ff9676dff
Bárbara Cristina Palma Cabral da Conceição <barbaracabraldaconceicao@MacBook-Air-de-Barbara.local>
2026-08-21 13:08:06 -0300

    Add reports

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 /* This test is a simple example of how to use Playwright to test a web page.
- * It demonstrates basic navigation and assertion techniques.
+ * It demonstrates basic navigation and assertion techniques WITHOUT using a page object model. The test navigates to the Playwright website, verifies the page title, clicks on the "Get started" link, and checks for the visibility of the "Installation" heading.
  */
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Playwright Page ', () => {
+  test('should have a title', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('should have a get started link', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
 });
--- tests/e2e/playwright-top-menu.spec.ts
+++ tests/e2e/playwright-top-menu.spec.ts
@@ -4,7 +4,7 @@ import { PlaywrightPage } from '../pages/playwright-page';
 /* This test is a simple example of how to use Playwright to test a web page.
  * It uses the PlaywrightPage class to interact with the page and verify that certain elements are visible and clickable.
  */
-test.describe('playwright top menu', () => {
+test.describe('Playwright Top Menu', () => {
   let playwrightPage: PlaywrightPage;
 
   test.beforeEach(async ({ page }) => {
@@ -12,7 +12,7 @@ test.describe('playwright top menu', () => {
     await playwrightPage.goto();
   });
 
-  test('shows the top menu links', async () => {
+  test('should show the top menu links', async () => {
     await playwrightPage.verifyLinkVisible('Docs');
     await playwrightPage.verifyTextIsVisible('MCP');
     await playwrightPage.verifyLinkVisible('API');
